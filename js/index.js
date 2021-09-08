36f40d3528e5743e0b236073b057608ecb9fd4ad
wellingtonnicacio444 <wellingtonngc12345@gmail.com>
2021-09-08 08:44:44 -0300

    adicionando cadastro

--- js/index.js
+++ js/index.js
@@ -6,3 +6,31 @@ function sh() {
         statusSenha.type = "password";
     }
 }
+function cadastro() {
+    let nome = String(formLogin.nome.value);
+    let password = document.getElementById("senha").value;
+    let email = document.getElementById("email").value;
+    if (nome === "") {
+        document.getElementById("nome").focus();
+    } else if (password === "") {
+        document.getElementById("senha").focus();
+        if (password.length < 4) {
+            document.getElementById("senha").focus();
+        } else {
+        }
+    } else {
+        if (email === "") {
+            document.getElementById("email").focus();
+        } else {
+            let regex = /\S+@\S+\.\S+/;
+            if (regex.test(email)) {
+                alert("Cadastro Aprovado!!");
+                document.getElementById("loginFinal");
+            }
+        }
+    }
+}
+
+function logar() {
+
+}
