fb681897008f2260f93f19e729e350f3a5ccacef
wellingtonnicacio444 <wellingtonngc12345@gmail.com>
2021-09-08 09:59:42 -0300

    concluindo tela de cadastro

--- js/index.js
+++ js/index.js
@@ -6,18 +6,19 @@ function sh() {
         statusSenha.type = "password";
     }
 }
+var senhaFinal;
+var usuarioFinal;
 function cadastro() {
     let nome = String(formLogin.nome.value);
     let password = document.getElementById("senha").value;
     let email = document.getElementById("email").value;
+    let convert = password.split("");
     if (nome === "") {
         document.getElementById("nome").focus();
     } else if (password === "") {
         document.getElementById("senha").focus();
-        if (password.length < 4) {
-            document.getElementById("senha").focus();
-        } else {
-        }
+    } else if (convert.length < 4) {
+        document.getElementById("senha").focus();
     } else {
         if (email === "") {
             document.getElementById("email").focus();
@@ -25,12 +26,23 @@ function cadastro() {
             let regex = /\S+@\S+\.\S+/;
             if (regex.test(email)) {
                 alert("Cadastro Aprovado!!");
-                document.getElementById("loginFinal");
+                // document.getElementById("loginFinal").innerText = "teste";
+                senhaFinal = password;
+                usuarioFinal = nome;
+                window.open('http://www.google.com.br', '_self');
+            } else {
+                alert("Preencha o email corretamente!!");
             }
         }
     }
 }
-
 function logar() {
+    alert(senhaFinal);
+    alert(usuarioFinal);
+    let senhaAtual = document.getElementById("senha").value;
+    if (senhaFinal != senhaAtual) {
+        alert("Digite a senha que você cadastrou anteriormente!!");
+    } else {
 
+    }
 }
